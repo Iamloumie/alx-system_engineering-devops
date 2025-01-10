2df1e242c8c5ecad20575b218981c10735ceaedc
Iamloumie <lawaladedamolaolumide@gmail.com>
2025-01-10 08:47:01 +0100

    added the arrow indentations

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,8 @@
 file {'/tmp/school':
-	ensure => file,
-	path => '/tmp/school',
-	owner => 'www-data',
-	group => 'www-data',
-	content => 'I love Puppet',
-	mode => '0744',
+ensure  => file,
+path    => '/tmp/school',
+owner   => 'www-data',
+group   => 'www-data',
+content => 'I love Puppet',
+mode    => '0744',
 }
