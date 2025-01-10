59c2c3bfabd79c0534e123e91dae59ecb7b6ba44
Iamloumie <lawaladedamolaolumide@gmail.com>
2025-01-10 12:50:38 +0100

    fixed

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,11 @@
-# Using Puppet, install flask from pip3.
+# Using Puppet to install packages.
 
 package { 'flask':
 ensure   => '2.1.0',
 provider => 'pip3',
+}
+
+package { 'werkzeug':
+ensure   => '2.1.1',
+provider => 'pip3'
 }
