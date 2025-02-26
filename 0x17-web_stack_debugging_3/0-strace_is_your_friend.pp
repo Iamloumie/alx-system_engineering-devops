bc060b21d7c0a9fb257ceadcc6b20fd1757f3b2d
Iamloumie <lawaladedamolaolumide@gmail.com>
2025-02-26 17:56:01 +0100

    fixed

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
 # fix error line (to find out why Apache is returning a 500 error)
 
-exec { 'replace':
-  path    => ['/bin', '/usr/bin'],
-  command => 'sudo sed -i "s/.phpp/.php/" /var/www/html/wp-settings.php',
+exec {'replace':
+  path    => ['/bin','/usr/bin'],
+  command => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php'
 }
