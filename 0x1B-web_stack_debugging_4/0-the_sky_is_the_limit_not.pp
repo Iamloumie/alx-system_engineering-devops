4bf578a5f1db49c6fb991a51638d178ec1600334
Iamloumie <lawaladedamolaolumide@gmail.com>
2025-03-06 11:38:47 +0100

    webstack debugging 4

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,11 @@
+# increase the amount of traffic nginx can handle
+
+# increase the ULIMIT of the nginx default
+exec { 'increase-ulimit':
+	command => '/bin/sed -i \'s/ULIMIT="-n 15"/ULIMIT="-n 4096"/\' /etc/default/nginx'
+}
+
+# restart nginx
+exec { 'restart-nginx':
+	command => '/usr/sbin/service nginx restart'
+}
