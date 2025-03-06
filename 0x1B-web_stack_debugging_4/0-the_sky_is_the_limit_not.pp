2ac2ec539eaf30b1029198293aa1255e86c5c37f
Iamloumie <lawaladedamolaolumide@gmail.com>
2025-03-06 11:42:11 +0100

    puppet-lint spaces

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -2,10 +2,10 @@
 
 # increase the ULIMIT of the nginx default
 exec { 'increase-ulimit':
-	command => '/bin/sed -i \'s/ULIMIT="-n 15"/ULIMIT="-n 4096"/\' /etc/default/nginx'
+    command => '/bin/sed -i \'s/ULIMIT="-n 15"/ULIMIT="-n 4096"/\' /etc/default/nginx'
 }
 
 # restart nginx
 exec { 'restart-nginx':
-	command => '/usr/sbin/service nginx restart'
+    command => '/usr/sbin/service nginx restart'
 }
--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -2,12 +2,12 @@
 
 # increase hard file limit for holberton user
 exec { 'increase-hard-limit-for-holberton-user':
-	command => 'sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf',
-	path	=> '/usr/bin/:/bin/',
+    command => 'sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf',
+    path    => '/usr/bin/:/bin/',
 }
 
 # increase soft file limit for holberton user
 exec { 'increase-soft-limit-for-holberton-user':
-	command => 'sed -i "/holberton soft/s/5/50000/" /etc/security/limits.conf',
-	path	=> '/usr/bin/:/bin/',
+    command => 'sed -i "/holberton soft/s/5/50000/" /etc/security/limits.conf',
+    path    => '/usr/bin/:/bin/',
 }
